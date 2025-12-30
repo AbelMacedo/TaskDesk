76674db75ee2061451c950ec21dd3a116751ffa3
Abel Macedo <perezmacedoabelyahir@gmail.com>
2025-12-29 20:35:19 -0600

    Agregar archivos de proyecto.

---
+++ TaskDesk/Data/Database.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace TaskDesk.Data
+{
+    internal static class Database
+    {
+        private static readonly string connectionString =
+            "Server=Abel\\SQLEXPRESS;Database=TaskDeskDB;Trusted_Connection=True;";
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
+
